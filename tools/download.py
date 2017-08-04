22ef1c8308b72bff180cd26600029ea239bb38b5
Chen2x <chenchen199727@gmail.com>
2017-08-04 10:09:52 -0400

    getting ready for testing

--- tools/download.py
+++ tools/download.py
@@ -25,21 +25,33 @@ cwd = os.getcwd()
         type: array
 """
 donor = task_dict.get('input').get('donor')
-vcflist = task_dict.get('input').get('vcflist')
+#vcflist = task_dict.get('input').get('vcflist')
 object_id = task_dict.get('input').get('object_id')
 tumor_id = task_dict.get('input').get('tumor_id')
-vcf = json.loads(task_dict.get('input').get('associatedVcfs'))
+vcf = json.loads(task_dict.get('input').get('vcflist'))
 
+#temp hard code (obj ids may not line up correctly just for test)
+objid = {"f8f0136b-09ec-d079-e040-11ac0c4842e7.broad-mutect-v3.20160222.somatic.snv_mnv.vcf.gz":'6963fa25-5037-50d8-bb15-4e3e9e891d86',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.broad-dRanger_snowman-10.20160101.somatic.sv.vcf.gz":'79286996-7da5-52b6-a21e-392e7d8489fa',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.broad-snowman-10.20160101.somatic.indel.vcf.gz":'400f343a-149c-54c5-aeef-d649ea3a6577',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.dkfz-snvCalling_1-0-132-1-hpc.1602051320.somatic.snv_mnv.vcf.gz":'100c52c6-d5cf-5720-91fb-c5d4a7ae165e',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.dkfz-indelCalling_1-0-132-1-hpc.1602051320.somatic.indel.vcf.gz":'b669398b-e461-5359-8e83-fff07e391c5e',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.embl-delly_1-0-0-preFilter-hpc.150901.somatic.sv.vcf.gz":'cff611fb-96dd-5fd9-9b50-43083177e828',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.MUSE_1-0rc-b391201-vcf.20160101.somatic.snv_mnv.vcf.gz":'acad417a-20d1-5a94-89a3-fc4bd9c3b863',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.svcp_1-0-5.20150306.somatic.snv_mnv.vcf.gz":'595c7ddb-dcde-5867-9b63-74bb1b23cfe4',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.svcp_1-0-5.20150306.somatic.indel.vcf.gz":'6508ab98-9733-5cf3-90be-45aac84de77b',
+        "f8f0136b-09ec-d079-e040-11ac0c4842e7.svfix2_4-0-12.20160213.somatic.sv.vcf.gz":'08bccc42-4a01-5a76-a911-ea5544d1c725'}
 
 task_start = int(time.time())
 
 try:
     os.mkdir(donor)
+    for i in vcf:
+        #include a get object ID step
+        k = subprocess.check_output(['icgc-storage-client', '--profile', 'collab', 'download', '--object-id', tumor_id, '--out-dir', donor])
+
     r = subprocess.check_output(['icgc-storage-client', '--profile', 'collab', 'download', '--object-id', object_id, '--out-dir', donor])
     f = subprocess.check_output(['icgc-storage-client', '--profile', 'collab', 'download', '--object-id', tumor_id, '--out-dir', donor])
-    for i in vcf:
-        #get object ID
-        k = subprocess.check_output(['icgc-storage-client', '--profile', 'collab', 'download', '--object-id', tumor_id, '--out-dir', donor])
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
     sys.exit(1)  # task failed
