95d50fd148c105f8c1a8e9fcf007c167f17ec143
Chen2x <chenchen199727@gmail.com>
2017-08-16 09:59:49 -0400

    syntax

--- oxog_varbam_annotate_wf_runner.0.1.jtracker/workflow/tools/setup_downloads.py
+++ oxog_varbam_annotate_wf_runner.0.1.jtracker/workflow/tools/setup_downloads.py
@@ -51,7 +51,7 @@ try:
         prog = 0
         urllib.urlretrieve(refUrl,'public_full9.tar.gz', reporthook=report)
         if refdone == True:
-        print(subprocess.check_output(['tar', 'xvzf', 'public_full9.tar.gz', '--directory', 'ref']))
+            print(subprocess.check_output(['tar', 'xvzf', 'public_full9.tar.gz', '--directory', 'ref']))
 
     refdir = os.path.abspath("ref")
 
