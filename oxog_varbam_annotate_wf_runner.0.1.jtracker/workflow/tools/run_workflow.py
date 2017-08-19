35c4b039899b356cce2f674c00642c2cd7e18a34
Chen2x <chenchen199727@gmail.com>
2017-08-19 01:55:11 -0400

    full test

--- oxog_varbam_annotate_wf_runner.0.1.jtracker/workflow/tools/run_workflow.py
+++ oxog_varbam_annotate_wf_runner.0.1.jtracker/workflow/tools/run_workflow.py
@@ -81,7 +81,7 @@ try:
     print(subprocess.check_output(['git', 'submodule', 'update', '--init', '--recursive']))
     os.chdir(cwd)
     print(subprocess.check_output(['cwltool', '--non-strict', '--relax-path-checks', 'OxoG-Dockstore-Tools/oxog_varbam_annotate_wf.cwl', 'run.json']))
-    cwltool --non-strict --relax-path-checks OxoG-Dockstore-Tools/oxog_varbam_annotate_wf.cwl run.json
+    #cwltool --non-strict --relax-path-checks OxoG-Dockstore-Tools/oxog_varbam_annotate_wf.cwl run.json
 
     # r = subprocess.check_output(['dockstore', '--script', '--debug', 'workflow', 'launch', '--descriptor', 'cwl', '--local-entry', '--entry', './oxog_varbam_annotate_wf.cwl', '--json', 'run.json'])
 except Exception, e:
